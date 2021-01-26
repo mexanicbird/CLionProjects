1566187b49eebf74890b723f1363a6810f50c94b
Алексей Куликов <alex@192.168.1.3>
2021-01-26 21:56:49 +0300

    commit_000_25.01.2021

---
+++ example_000/cmake-build-debug/CMakeFiles/example_000.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example_000.dir/main.c.o"
+  "example_000"
+  "example_000.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/example_000.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ example_000/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    int age;
+    printf("Hi FOX! \n");
+    printf("Input your age:");
+    scanf("%d", &age);
+    printf("age = %d", age);
+    return 0;
+
+}
