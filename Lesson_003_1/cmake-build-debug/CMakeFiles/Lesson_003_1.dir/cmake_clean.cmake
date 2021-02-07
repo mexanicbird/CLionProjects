5b74129d3d5d3485f1bd0b7423302e97171ee68b
Алексей Куликов <alex@MacBook-Pro-Aleksej.local>
2021-02-07 21:27:12 +0300

    commit_004_07.02.2021

---
+++ Lesson_003_1/cmake-build-debug/CMakeFiles/Lesson_003_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lesson_003_1.dir/main.c.o"
+  "Lesson_003_1"
+  "Lesson_003_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lesson_003_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
